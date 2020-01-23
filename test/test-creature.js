062a0b270ce297b0af63f8b5356b64e442ee7ef5
Yuet Loo Wong <contact@yuetloo.com>
2020-01-23 13:28:24 -0500

    add mintTo test case

--- test/test-creature.js
+++ test/test-creature.js
@@ -1,22 +1,29 @@
 const Creature = artifacts.require("Creature");
+const ethers = require('ethers');
 
 contract("Creature", accounts => {
-  it("should have totalSupply of 0", () =>
-  { 
-    return Creature.deployed()
-        .then( instance => instance.totalSupply())
-        .then(supply => {
-            assert.equal(
-            supply.valueOf(),
-            0,
-            "total supply is not zero"
-            );
-        });
+    let instance;
+    before(() => {
+        return Creature.deployed()
+            .then(contractInstance => {
+                instance = contractInstance;
+            });
     });
+
+    it("should have totalSupply of 0", () =>
+    {
+        return instance.totalSupply()
+            .then(supply => {
+                assert.equal(
+                supply.valueOf(),
+                0,
+                "total supply is not zero"
+                );
+            });
+        });
     it("should have name as Creature", () =>
-    { 
-      return Creature.deployed()
-          .then( instance => instance.name())
+    {
+      return instance.name()
           .then(name => {
               assert.equal(
               name,
@@ -24,5 +31,18 @@ contract("Creature", accounts => {
               "name is not creature"
               );
           });
-      });
+    });
+    it("should be able to mint successfully", () =>
+    {
+        const newAddress = ethers.Wallet.createRandom().address;
+        return instance.mintTo(newAddress)
+            .then(() => instance.totalSupply())
+            .then(supply => {
+                assert.equal(
+                supply.valueOf(),
+                1,
+                "total supply is not 1"
+                );
+            });
+    });
 });
